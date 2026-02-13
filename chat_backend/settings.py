14d3f188ee493f47dda184c8ac616c42c9f85cc8
jenilpatel252525-cyber <jenilpatel252525@gmail.com>
2026-02-14 00:06:53 +0530

    commit

--- chat_backend/settings.py
+++ chat_backend/settings.py
@@ -85,7 +85,10 @@ ASGI_APPLICATION = "chat_backend.asgi.application"
 CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
-        "CONFIG": {"hosts": [("127.0.0.1", 6379)]},
+        # "CONFIG": {"hosts": [("127.0.0.1", 6379)]},
+        "CONFIG": {
+            "hosts": [os.environ.get("REDIS_URL")],
+        },
     }
 }
 
